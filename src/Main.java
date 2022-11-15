ab5c60d71a939104802aa1f5c5717279fdbbe9bb
tatiana_alekseeva <tatianaalekseeva1313@gmail.com>
2022-11-15 09:03:33 +0300

    Проверьте дз

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        // Задача 1
+        //Задача 1
         System.out.println("Задача 1");
         int startYear = 1822;
         int endYear = 2122;
@@ -9,7 +9,7 @@ public class Main {
                 System.out.println(startYear);
             }
         }
-        // Задача 2
+        //Задача 2
         System.out.println("Задача 2");
         byte a = 2;
         for(int i = 1; i <= 10; i++) {
